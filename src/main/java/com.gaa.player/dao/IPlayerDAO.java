42cf0b27f06fb5a905ae43436403ced9d7222fd8
Adam Cassidy <D00265095@student.dkit.ie>
2025-08-19 22:37:29 +0100

    start

--- src/main/java/com.gaa.player/dao/IPlayerDAO.java
+++ src/main/java/com.gaa.player/dao/IPlayerDAO.java
@@ -1,4 +1,14 @@
 package com.gaa.player.dao;
 
-public class IPlayerDAO {
+import com.gaa.player.dto.PlayerDTO;
+import java.util.List;
+
+public interface IPlayerDAO {
+    List<PlayerDTO> getAllPlayers();
+    PlayerDTO getPlayerById(int id);
+    boolean deletePlayerById(int id);
+    PlayerDTO insertPlayer(PlayerDTO player);
+    List<PlayerDTO> findPlayersUsingFilter(String filter);
+    String findAllPlayersJson();
+    String findPlayerByIdJson(int id);
 }
--- src/main/java/com.gaa.player/dto/PlayerDTO.java
+++ src/main/java/com.gaa.player/dto/PlayerDTO.java
@@ -1,4 +1,74 @@
 package com.gaa.player.dto;
 
+import com.gaa.player.model.Player;
+
 public class PlayerDTO {
+    private int id;
+    private String name;
+    private int age;
+    private float height;
+    private float weight;
+    private String position;
+    private String county;
+    private String club;
+    private int goalsScored;
+    private int pointsScored;
+    private boolean isActive;
+
+    public PlayerDTO() {}
+
+    public PlayerDTO(Player player) {
+        this.id = player.getId();
+        this.name = player.getName();
+        this.age = player.getAge();
+        this.height = player.getHeight();
+        this.weight = player.getWeight();
+        this.position = player.getPosition();
+        this.county = player.getCounty();
+        this.club = player.getClub();
+        this.goalsScored = player.getGoalsScored();
+        this.pointsScored = player.getPointsScored();
+        this.isActive = player.isActive();
+    }
+
+    // Getters and setters for all fields
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public float getHeight() { return height; }
+    public void setHeight(float height) { this.height = height; }
+    public float getWeight() { return weight; }
+    public void setWeight(float weight) { this.weight = weight; }
+    public String getPosition() { return position; }
+    public void setPosition(String position) { this.position = position; }
+    public String getCounty() { return county; }
+    public void setCounty(String county) { this.county = county; }
+    public String getClub() { return club; }
+    public void setClub(String club) { this.club = club; }
+    public int getGoalsScored() { return goalsScored; }
+    public void setGoalsScored(int goalsScored) { this.goalsScored = goalsScored; }
+    public int getPointsScored() { return pointsScored; }
+    public void setPointsScored(int pointsScored) { this.pointsScored = pointsScored; }
+    public boolean isActive() { return isActive; }
+    public void setActive(boolean active) { isActive = active; }
+
+    @Override
+    public String toString() {
+        return "PlayerDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", position='" + position + '\'' +
+                ", county='" + county + '\'' +
+                ", club='" + club + '\'' +
+                ", goalsScored=" + goalsScored +
+                ", pointsScored=" + pointsScored +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
